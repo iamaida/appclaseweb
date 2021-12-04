8911b326b4715565e4400759754633c6fd7c38ed
Camilo Mezú Mina <camilo272001@gmail.com>
2021-12-03 20:10:44 -0500

    peticion  POST endpoint CAMILO

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get('/prueba',(req, res) => {
     res.send("Prueba Superada")
 } )
 
+app.post('/camilo', (req, res) => {
+    res.json({
+        "cedula": "123456789",
+        "nombre": "camilo"
+    })
+})
 
 app.listen(3000,()=>{
     console.log("Conectado Puerto 3000")
