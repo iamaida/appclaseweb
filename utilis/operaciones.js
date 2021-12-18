ab912c1774aba1339b2ea0094100596c383308d1
Jose Manuel Martinez <51792325+JoseMMC99@users.noreply.github.com>
2021-12-18 14:49:36 -0500

    Punto 3. Primos

--- utilis/operaciones.js
+++ utilis/operaciones.js
@@ -1,56 +1,68 @@
 function pares(x) {
-    let resp = ""
-    if (x<2){
-     resp="Los numeros son : 0"
-     return resp
-    }
-
-   resp = "Los numeros son :"
-   let i = 0;
-   do {
-   if ( i % 2 == 0){
-    resp = resp + ` ${i}`
-   }   
-   i = i + 1;
-   } while (i <= x);
-
+  let resp = "";
+  if (x < 2) {
+    resp = "Los numeros son : 0";
     return resp;
- }
+  }
 
- function factor(x) { //Saca el numero en factorial
-	if (x == 0){ 
-		return 1; 
-	}
-	return x * factor (x-1); 
+  resp = "Los numeros son :";
+  let i = 0;
+  do {
+    if (i % 2 == 0) {
+      resp = resp + ` ${i}`;
+    }
+    i = i + 1;
+  } while (i <= x);
+
+  return resp;
 }
 
-function factorial(x){//Da el resultado factorial de un numero
-    let resp = "El resultado factorial es : "
-    let fact = ""
-    fact = factor(x)
-    return resp + fact;
+function factor(x) {
+  //Saca el numero en factorial
+  if (x == 0) {
+    return 1;
+  }
+  return x * factor(x - 1);
 }
 
-function seriefactorial(x){ //Hace la serie factorial s = 1! - 2! + 3! -4! ... n!
-    let resp = "El resultado de la serie factorial es : "
-    let fact = ""
-    let op = ""
-   let i = 1;
-   do {
-    fact = factor(i)
-   if ( i % 2 == 0){
-    op = op - fact
-   }
-   else {
-    op = op + fact
-   }
-   i = i + 1;
-   } while (i <= x);
-    return resp + op;
+function primos(x) {
+  let primos = [];
+  for (let i = 2; i < x; i++) {
+    if (num % 1 === 0) return false;
+    primos.push(num);
+  }
+  return primos;
 }
 
- module.exports={
-     pares,
-     seriefactorial,
-     factorial
- }
+function factorial(x) {
+  //Da el resultado factorial de un numero
+  let resp = "El resultado factorial es : ";
+  let fact = "";
+  fact = factor(x);
+  return resp + fact;
+}
+
+function seriefactorial(x) {
+  //Hace la serie factorial s = 1! - 2! + 3! -4! ... n!
+  let resp = "El resultado de la serie factorial es : ";
+  let fact = "";
+  let op = "";
+  let i = 1;
+  do {
+    fact = factor(i);
+    if (i % 2 == 0) {
+      op = op - fact;
+    } else {
+      op = op + fact;
+    }
+    i = i + 1;
+  } while (i <= x);
+  return resp + op;
+}
+
+module.exports = {
+  pares,
+  primos,
+  seriefactorial,
+  factorial,
+};
